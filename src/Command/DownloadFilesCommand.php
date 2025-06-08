af62b6962ae9d9a1fc944d88809fba6e0e6a4e2e
r.ziedins <r.ziedins@dynatech.lv>
2025-06-08 21:30:27 +0300

    Added file downloading & progress output

--- src/Command/DownloadFilesCommand.php
+++ src/Command/DownloadFilesCommand.php
@@ -12,49 +12,147 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[AsCommand(name: 'app:download-files')]
 class DownloadFilesCommand extends Command
 {
+    private const OUTPUT_DIRECTORY = __DIR__ . '/../../downloads';
+    private const KEY_FILE_RESOURCE = 'fileResource';
+    private const KEY_FILE_PATH = 'filePath';
+    private const KEY_CONTINUE = 'continue';
+    private const KEY_STATUS = 'status';
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $urls = $input->getArgument('urls');
+        $this->prepareDirectories();
 
+        $urls = $input->getArgument('urls');
+        try {
+            $results = $this->downloadUrls($urls);
+        } catch (\Exception $e) {
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+
+            return Command::FAILURE;
+        }
+
+        foreach ($results as $key => $result) {
+            $output->writeln(sprintf(
+                "%d: Response code : %d | %s | %s",
+                $key,
+                $result[self::KEY_STATUS],
+                $result[self::KEY_CONTINUE] ? 'Continued' : 'Not Continued',
+                $result[self::KEY_FILE_PATH]
+            ));
+        }
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @param array $urls
+     * @return array
+     * @throws \Exception
+     */
+    private function downloadUrls(array $urls): array
+    {
         $multiHandle = curl_multi_init();
-        $curlHandles = [];
+        $curlHandles = new \WeakMap();
 
         foreach ($urls as $url) {
+            $fileName = $this->getFileNameFromUrl($url);
+            $filePath = self::OUTPUT_DIRECTORY . '/' . $fileName;
+            $continue = false;
+            $lastByte = file_exists($filePath) ? filesize($filePath) : 0;
+            $fileResource = fopen($filePath, 'a+');
+
             $curlHandle = curl_init();
+
+            if ($lastByte > 0) {
+                curl_setopt($curlHandle, CURLOPT_RANGE, "$lastByte-");
+                $continue = true;
+            }
+
             curl_setopt_array($curlHandle, [
-                CURLOPT_URL => $url,
-                CURLOPT_RETURNTRANSFER => true,
-//                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_URL              => $url,
+                CURLOPT_FILE             => $fileResource,
+                CURLOPT_CONNECTTIMEOUT   => 3,
+                CURLOPT_NOPROGRESS       => false,
+                CURLOPT_HEADER           => false,
+                CURLOPT_PROGRESSFUNCTION => static function ($curlHandle, $bytesToDownload, $bytesDownloaded) use (
+                    $url,
+                    $continue
+                ) {
+                    static $lastProgress = [];
+
+                    if ($bytesToDownload > 0) {
+                        if ($continue) {
+                            echo basename($url) . ' Resuming Download: ' . $bytesDownloaded . '/' . $bytesToDownload . "\n";
+                        }
+
+                        $percent = round(($bytesDownloaded / $bytesToDownload) * 100);
+                        if (!isset($lastProgress[$url]) || $percent - $lastProgress[$url] >= 5) {
+                            echo basename($url) . ' Downloaded: ' . $percent . "%\n";
+                            $lastProgress[$url] = $percent;
+                        }
+                    }
+
+                    return 0;
+                },
             ]);
+
             curl_multi_add_handle($multiHandle, $curlHandle);
-            $curlHandles[] = $curlHandle;
+            $curlHandles[$curlHandle] = [
+                self::KEY_FILE_RESOURCE => $fileResource,
+                self::KEY_FILE_PATH     => $filePath,
+                self::KEY_CONTINUE     => $continue,
+            ];
         }
 
         $stillRunning = 0;
+
         do {
             $status = curl_multi_exec($multiHandle, $stillRunning);
-            curl_multi_select($multiHandle);
-        } while ($stillRunning > 0 && $status == CURLM_OK);
+            if ($stillRunning) {
+                curl_multi_select($multiHandle);
+            }
+        } while ($stillRunning && $status == CURLM_OK);
 
-        foreach ($curlHandles as $key => $curlHandle) {
-            $content = curl_multi_getcontent($curlHandle);
-            $error = curl_error($curlHandle);
-            $info = curl_getinfo($curlHandle);
+        $results = [];
+
+        foreach ($curlHandles as $curlHandle => $handleData) {
+            fclose($handleData[self::KEY_FILE_RESOURCE]);
             curl_multi_remove_handle($multiHandle, $curlHandle);
+            $status = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
             curl_close($curlHandle);
 
-            if ($error !== '') {
-                $output->writeln("<error>[{$key}] Error: $error</error>");
-            } else {
-                $url = $info['url'] ?? 'Url not set';
-                $output->writeln("<info>[{$key}] $url</info>");
-                $output->writeln(substr($content, 0, 200) . "\n");
-            }
+            $results[] = [
+                self::KEY_STATUS => $status,
+                self::KEY_FILE_PATH => $handleData[self::KEY_FILE_PATH],
+                self::KEY_CONTINUE => $handleData[self::KEY_CONTINUE]
+            ];
         }
 
         curl_multi_close($multiHandle);
 
-        return Command::SUCCESS;
+        return $results;
+    }
+
+    /**
+     * @param string $url
+     * @return string
+     * @throws \Exception
+     */
+    private function getFileNameFromUrl(string $url): string
+    {
+        $urlPath = parse_url($url, PHP_URL_PATH);
+        if (!$urlPath) {
+            throw new \Exception("Invalid url: " . $url . " The File url must be provided like so example.com/path/to/file");
+        }
+
+        return basename($urlPath) ?: md5($url);
+    }
+
+    private function prepareDirectories(): void
+    {
+        if (!is_dir(self::OUTPUT_DIRECTORY)) {
+            mkdir(self::OUTPUT_DIRECTORY, 0777, true);
+        }
     }
 
     protected function configure(): void
